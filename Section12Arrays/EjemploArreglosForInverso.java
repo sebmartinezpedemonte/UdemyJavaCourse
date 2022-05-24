0f9e0e6f631a44bf485c2e7327b1c0591b06c332
Sebastian Martinez <sebastianmartinezpedemonte@gmail.com>
2022-05-24 16:40:20 -0300

    May 24th

---
+++ Section12Arrays/EjemploArreglosForInverso.java
@@ -0,0 +1,44 @@
+package Section12Arrays;
+
+import java.util.Arrays;
+
+public class EjemploArreglosForInverso {
+    public static void main(String[] args) {
+
+
+        String[] productos = new String[7];
+        //Tambien se puede declara asi:
+        /*
+        String[] productos = {"Kingston Pendrive 64GB","Samsung Galaxy",
+                "Disco Duro SSD Samsung Externo","Asus Notebook", "MacBook Air",
+                "Chromecast 4ta generacion", "Bicileta Oxford" };
+        */
+        int total = productos.length;
+
+
+        productos[0] = "Kingston Pendrive 64GB";
+        productos[1] = "Samsung Galaxy";
+        productos[2] = "Disco Duro SSD Samsung Externo";
+        productos[3] = "Asus Notebook";
+        productos[4] = "MacBook Air";
+        productos[5] = "Chromecast 4ta generacion";
+        productos[6] = "Bicileta Oxford";
+
+
+        //ordenar antes de iterar o mostrar
+        Arrays.sort(productos); //lo ordena de la A la Z
+        System.out.println("=== usando for  ===");
+        for (int i = 0; i < total; i++) {
+            System.out.println("Para indice " + i + " : " + productos[i]);
+        }
+        System.out.println("=== Usando for inverso===");
+        for(int i = 0; i < total; i++){
+            System.out.println("i = " + (total -1 - i) + " valor: " +productos[total-1 - i]);
+        }
+        System.out.println("=== Usando for inverso 2 ===");
+        for(int i = total -1; i >=0; i--){
+            System.out.println("para i = " + i + " valor " + productos[i]);
+        }
+
+    }
+}
---
+++ Section12Arrays/EjemploArreglosForOrdenamientoBurbuja.java
@@ -0,0 +1,41 @@
+package Section12Arrays;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class EjemploArreglosForOrdenamientoBurbuja {
+    public static void arregloInverso(String[] arreglo) {
+        int total = arreglo.length;
+        int total2 = arreglo.length;
+        for (int i = 0; i < total2; i++) {
+            String actual = arreglo[i];
+            String inverso = arreglo[total - 1 - i];
+            arreglo[i] = inverso;
+            arreglo[total - 1 - i] = actual;
+            total2--;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        String[] productos = {"Kingston Pendrive 64GB", "Samsung Galaxy",
+                "Disco Duro SSD Samsung Externo", "Asus Notebook", "MacBook Air",
+                "Chromecast 4ta generacion", "Bicileta Oxford"};
+
+        int total = productos.length;
+        //ordenar antes de iterar o mostrar
+        //Arrays.sort(productos); //lo ordena de la A la Z
+        //arregloInverso(productos);  //lo ordena de la forma inversa de la Z a la A
+
+
+
+        //otra forma es usando la Java Collections API
+        Collections.reverse(Arrays.asList(productos));
+        System.out.println("=== usando for  ===");
+        for (int i = 0; i < total; i++) {
+            System.out.println("Para indice " + i + " : " + productos[i]);
+        }
+
+
+    }
+}
