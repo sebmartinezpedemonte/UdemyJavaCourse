de1c6bd8ad3a80e9fffd9d4da5db7bd69474f5a9
sebmartinezpedemonte <sebastianmartinezpedemonte@gmail.com>
2022-10-05 19:34:40 -0300

    archivos

--- section33/manejodearchivos/ejemplos/EjemploLeerArchivo.java
+++ section33/manejodearchivos/ejemplos/EjemploLeerArchivo.java
@@ -3,14 +3,13 @@ package section33.manejodearchivos.ejemplos;
 import section33.manejodearchivos.ejemplos.servicio.ArchivoServicio;
 import section33.manejodearchivos.ejemplos.servicio.ArchivoServicioBufferedWriter;
 
-public class EjemploCrearBufferedWriter {
+public class EjemploLeerArchivo {
     public static void main(String[] args) {
 
+        ArchivoServicioBufferedWriter servicio = new ArchivoServicioBufferedWriter();
         String nombreArchivo = "C:\\Cursos\\Java\\archivo\\java.txt";
-
-        ArchivoServicioBufferedWriter service = new ArchivoServicioBufferedWriter();
-        //service.crearArchivo(nombreArchivo);
-        service.crearArchivo2(nombreArchivo);
-
+        System.out.println(servicio.leerArchivo(nombreArchivo));
+        System.out.println("-".repeat(80));
+        System.out.println(servicio.leerArchivo2(nombreArchivo));
     }
 }
--- section33/manejodearchivos/ejemplos/servicio/ArchivoServicioBufferedWriter.java
+++ section33/manejodearchivos/ejemplos/servicio/ArchivoServicioBufferedWriter.java
@@ -1,9 +1,7 @@
 package section33.manejodearchivos.ejemplos.servicio;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.util.Scanner;
 
 //io:input output
 public class ArchivoServicioBufferedWriter {
@@ -22,4 +20,52 @@ public class ArchivoServicioBufferedWriter {
         }
 
     }
+
+    //este nos sirve para poder usar la funcion print con todas sus
+    //modalidades: println, printf, print
+    public void crearArchivo2(String nombre){
+        File archivo = new File(nombre);
+        try (PrintWriter buffer = new PrintWriter(new FileWriter(archivo, true))) {
+            buffer.println("Hola que tal amigos!");
+            buffer.println("Todo bien? Yo aca escribiendo un archivo...");
+            buffer.printf("Hasta luego %s!", "Lucas");
+            //buffer.close();//usando este try no necesitamos usar el close
+            System.out.println("El archivo se ha creado con exito");
+        } catch (IOException e) {
+            e.printStackTrace();
+            //throw new RuntimeException(e);
+        }
+
+    }
+
+    public String leerArchivo(String nombre){
+        StringBuilder sb = new StringBuilder();
+        File archivo = new File(nombre);
+        try {
+            BufferedReader reader = new BufferedReader((new FileReader(archivo)));
+            String linea;
+            while((linea = reader.readLine()) != null){
+                sb.append(linea).append("\n");
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return sb.toString();
+    }
+
+    public String leerArchivo2(String nombre){
+        StringBuilder sb = new StringBuilder();
+        File archivo = new File(nombre);
+        try (Scanner s = new Scanner(archivo)){
+            s.useDelimiter("\n");
+            while(s.hasNext()){
+                sb.append(s.next()).append("\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return sb.toString();
+    }
+
 }
